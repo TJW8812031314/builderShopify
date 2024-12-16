fb5f9e1ff7af694f1e213efdd06aca11ba2bf38e
tianjiwen <1362368453@qq.com>
2024-12-16 16:29:36 +0800

    builder.io 测试3

---
+++ _nuxt/D2msIR2f.js
@@ -0,0 +1,1 @@
+import{Q as n,d as t,$ as o}from"./pz2FBFsi.js";const r=t({name:"awaiter",props:[],mounted(){}});function a(e,s,d,f,m,p){return o(e.$slots,"default")}const i=n(r,[["render",a]]);export{i as default};
